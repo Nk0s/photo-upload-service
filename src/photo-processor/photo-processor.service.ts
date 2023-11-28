dfae542b8c57d14c753d8c8a0c942d50cc2e770c
Nkos <nkos.j@icloud.com>
2023-11-28 18:32:16 +0400

    error

--- src/photo-processor/photo-processor.service.ts
+++ src/photo-processor/photo-processor.service.ts
@@ -1,22 +1,19 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { InjectQueue, Process } from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
+import { InjectQueue, Process, Processor } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { Photo } from 'src/photo.model/photo.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-@Injectable()
+@Processor('photo')
 export class PhotoProcessor {
   private readonly logger = new Logger(PhotoProcessor.name);
 
   constructor(
     @InjectQueue('photo') private readonly photoQueue: Queue,
     @InjectModel(Photo.name) private readonly photoModel: Model<Photo>,
-  ) {
-    this.photoQueue.process('photo', this.handleProcessPhoto.bind(this));
-  }
-
-  @Process('photo')
+  ) {}
+  @Process()
   async handleProcessPhoto(job: any) {
     try {
       const { data } = job;
